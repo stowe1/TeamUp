7d305c86cb0577d623c647a80255902e50a30423
Tristan Floyd <tlfloyd2@gmail.com>
2024-04-12 21:54:40 -0500

    Test Top Bar Again

--- hackathon/src/screens/CommunityScreen.js
+++ hackathon/src/screens/CommunityScreen.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, TextInput, StyleSheet} from 'react-native';
 
 export default function CommunityScreen({navigation}) {
     return (
