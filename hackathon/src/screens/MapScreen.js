31976b7260fe02478806342a733fedba895331b6
stowe1 <sethmtowe@gmail.com>
2024-04-12 21:58:52 -0500

    maps

--- hackathon/src/screens/MapScreen.js
+++ hackathon/src/screens/MapScreen.js
@@ -1,10 +1,19 @@
 import * as React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import MapView from 'react-native-maps';
+
 
 export default function MapScreen({navigation}) {
     return (
         <View style={styles.container}>
-            <Text>Map Screen</Text>
+            <MapView
+  initialRegion={{
+    latitude: 37.78825,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  }}
+/>
         </View>
     );
 }
