f9dd64dd8dbfc0144a1e695270c2267c16f8dc46
Colin <colinmyers153@gmail.com>
2024-04-13 09:28:20 -0500

    Base Profile

---
+++ hackathon/src/constants/images.js
@@ -0,0 +1,7 @@
+const background = require("../../assets/background1.jpeg");
+const profile = require("../../assets/profile.jpg");
+
+export default {
+    background,
+    profile
+}
--- hackathon/src/screens/ProfileScreen.js
+++ hackathon/src/screens/ProfileScreen.js
@@ -1,18 +1,77 @@
-import * as React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Image, StyleSheet, TextInput } from 'react-native';
+import { SafeAreaView } from "react-native-safe-area-context";
+import { COLORS, images } from "../constants";
+import { StatusBar } from "expo-status-bar";
+import { MaterialIcons } from "@expo/vector-icons";
 
 export default function ProfileScreen({navigation}) {
+    const [inputName, setInputName] = useState('');
+    const [displayedName, setDisplayedName] = useState('');
+
+    const handleChangeName = (text) => {
+        setInputName(text);
+    };
+
+    const handleSubmitName = () => {
+        setDisplayedName(inputName);
+        setInputName('');
+    };
+
     return (
-        <View style={styles.container}>
-            <Text>Profile Screen</Text>
-        </View>
-    );
+        <SafeAreaView style={{
+            flex: 1,
+            backgroundColor: COLORS.white
+        }}>
+            <StatusBar backgroundColor={COLORS.GRAY} />
+            <View style={{ width: "100%" }}>
+                <Image
+                    source={images.background1}
+                    resizeMode='cover'
+                    style={{
+                        height: 228,
+                        width: "100%"
+                    }}
+                />
+            </View>
+
+            <View style={{ flex: 1, alignItems: "center" }}>
+                <Image
+                    source={images.profile}
+                    resizeMode='contain'
+                    style={{
+                        height: 150,
+                        width: 155,
+                        borderRadius: 999,
+                        borderColor: COLORS.NAVY_BLUE,
+                        borderWidth: 2,
+                        top: -185
+                    }}
+                />
+
+                <Text style={{
+                    color: COLORS.NAVY_BLUE,
+                    marginBottom: 10
+                }}>Name: {displayedName}</Text>
+
+                <TextInput
+                    style={styles.input}
+                    placeholder="Enter your name"
+                    onChangeText={handleChangeName}
+                    value={inputName}
+                    onSubmitEditing={handleSubmitName}
+                />
+            </View>
+        </SafeAreaView>
+    )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+    input: {
+        height: 40,
+        width: "80%",
+        borderColor: 'gray',
+        borderWidth: 1,
+        paddingHorizontal: 10,
     },
 });
